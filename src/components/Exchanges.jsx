5b10dfa4e4028faff23d9b843af7e3c8455946c1
Dhruv Krishnamachari <dhruv.rajeshk@gmail.com>
2022-01-17 13:59:31 +0530

    Update Exchanges.jsx

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,20 +1,7 @@
 import React from 'react'
-import millify from 'millify'
-import { Collapse, Row, Col, Typography, Avatar } from 'antd'
-import HTMLReactParser from 'html-react-parser'
-
-import { useGetExchangesQuery } from '../services/cryptoApi'
-import Loader from './Loader'
-
-const { Text, Title } = Typography
-const { Panel } = Collapse
+const { Title } = Typography
 
 const Exchanges = () => {
-  const { data, isFetching } = useGetExchangesQuery()
-  const exchangesList = data?.data?.exchanges
-
-  if (isFetching) return <Loader />
-
   return (
     {/*
     <>
