4144e459416d606fbdd4983a7c544f95971a7132
Dhruv Krishnamachari <dhruv.rajeshk@gmail.com>
2022-01-17 18:07:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from './App'
-import 'antd/dist/antd.css'
-import { Provider } from 'react-redux'
-import store from './app/store'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
+  <React.StrictMode>
     <Router>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </Router>, 
-    document.getElementById('root')
-)
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
